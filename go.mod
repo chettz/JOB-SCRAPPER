22afe20543ec6d88762d33683db6bf0242d789a6
chettz <chyohan85@gmail.com>
2025-05-13 19:26:25 +0900

    first commit

---
+++ go.mod
@@ -0,0 +1,12 @@
+module job-scrapper
+
+go 1.23.0
+
+toolchain go1.23.9
+
+require github.com/PuerkitoBio/goquery v1.10.3
+
+require (
+	github.com/andybalholm/cascadia v1.3.3 // indirect
+	golang.org/x/net v0.39.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"strconv"
+
+	"github.com/PuerkitoBio/goquery"
+)
+
+type extractedJob struct{
+	id		string
+	title	string
+	
+}
+
+var baseURL string = "https://www.saramin.co.kr/zf_user/search/recruit?=&searchword=python"
+
+
+func main() {
+	totalPages := getPages()
+
+	for i:=0;i<totalPages;i++{
+		getPage(i)
+	}
+}
+
+func getPage(page int){
+	pageURL := baseURL + "&recruitPage=" + strconv.Itoa(page)
+	fmt.Println("Requesting", pageURL)
+	res, err := http.Get(pageURL)
+	checkErr(err)
+	checkCode(res)
+
+	defer res.Body.Close()
+
+	doc, err := goquery.NewDocumentFromReader(res.Body)
+	checkErr(err)
+
+	searchCards := doc.Find(".item_recruit")
+
+	searchCards.Each(func(i int, s *goquery.Selection){ // 's' means each card section
+		id, _ := s.Attr("value")
+		fmt.Println(id)
+	})
+
+}
+
+func getPages() int {
+	pages := 0
+	res, err := http.Get(baseURL)
+	checkErr(err)
+	checkCode(res)
+
+	defer res.Body.Close()
+
+	doc, err := goquery.NewDocumentFromReader(res.Body)
+	checkErr(err)
+
+	doc.Find(".pagination").Each(func(i int, s *goquery.Selection){
+		pages = s.Find("a").Length()
+	})
+	return pages
+}
+
+func checkErr(err error){
+	if err != nil{
+		log.Fatalln(err)
+	}
+}
+
+func checkCode(res *http.Response){
+	if res.StatusCode != 200{
+		log.Fatalln("Request failed with Status:", res.StatusCode)
+	}
+}
